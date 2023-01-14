c6fa9119148df5b90fbe51018e9976bb0ba748b6
Bhargav Burgul <bhargavburgul@gmail.com>
2023-01-14 17:18:11 +0530

    updated server url

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-chat-eutc.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
